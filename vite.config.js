af116027719cc691b45c6cf84517293ae93f7513
Priya <bhardwajpriya402@gmail.com>
2025-09-07 10:35:53 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0",
+    port: 5173, 
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 4173, 
+  },
+});
